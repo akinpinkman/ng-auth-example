6019028a77cf6580357f8932b4c8a6953f99fe0d
akinpinkman <akinbilgin54@hotmail.com>
2024-09-16 22:47:32 +0300

    initial commit

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Subject, tap } from 'rxjs';
+import { User } from './user.model';
+
+export interface AuthResponse {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user = new BehaviorSubject<User>(null);
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponse>('http://localhost:3001/signup', {
+        email: email,
+        password: password,
+      })
+      .pipe(
+        tap((resData) => {
+          this.handleAuth(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponse>('http://localhost:3001/login', {
+        email: email,
+        password: password,
+      })
+      .pipe(
+        tap((resData) => {
+          this.handleAuth(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuth(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+}
