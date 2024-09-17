d39a4443165c148d73cb5f5210cdd51a13dbd1aa
akinpinkman <akinbilgin54@hotmail.com>
2024-09-17 12:56:41 +0300

    save user data to local storage and implement auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, Subject, tap } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponse {
   kind: string;
@@ -19,7 +20,7 @@ export interface AuthResponse {
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
     return this.http
@@ -57,6 +58,28 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(userData.email, userData.id, userData._token);
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
+  }
+
   private handleAuth(
     email: string,
     userId: string,
@@ -67,5 +90,6 @@ export class AuthService {
 
     const user = new User(email, userId, token);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 }
